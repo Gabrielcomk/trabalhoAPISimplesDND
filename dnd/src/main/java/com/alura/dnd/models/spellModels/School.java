d9bc4d40dc25f5056226fe77f5fd2df963a50c28
Gabriel <116319547+Gabrielcomk@users.noreply.github.com>
2025-10-24 10:06:41 -0300

    Delete dnd directory

--- dnd/src/main/java/com/alura/dnd/models/spellModels/School.java
+++
@@ -1,8 +0,0 @@
-package com.alura.dnd.models.spellModels;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record School(
-        String name
-) {}
